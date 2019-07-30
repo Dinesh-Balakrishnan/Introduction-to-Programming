6409d67f140a1b31e32d24d575095aa707c99b78
Dinesh-Balakrishnan <40499783+Dinesh-Balakrishnan@users.noreply.github.com>
2019-07-29 19:29:13 -0500

    Create 17.js

---
+++ Web-Programming/Notes/17.js
@@ -0,0 +1,38 @@
+/*
+  DEFINITIONS:
+
+  - Cross-Origin Resource Sharing (CORS): A mechanism that uses additional HTTP headers
+    to allow a web server to perform a cross-origin request.
+
+  - Cross-Origin HTTP Request: The web server requesting a resource from a different
+    origin than it's own.
+
+  - Origin: Web content have a scheme (protocol), host (domain), and port assigned
+  to them, all located in the URL.
+    - Scheme: HTTP, HTTPS
+    - Host: example.com
+    - Port: 80 (Default Port)
+*/
+
+/* Browsers automatically restrict cross-origin HTTP requests. It isn't possible to
+access local files either because they use file pathways rather than URLs. To access
+local files, a webserver needs to be created.*/
+/*
+  DEFINITIONS:
+
+  - Cross-Origin Resource Sharing (CORS): A mechanism that uses additional HTTP headers
+    to allow a web server to perform a cross-origin request.
+
+  - Cross-Origin HTTP Request: The web server requesting a resource from a different
+    origin than it's own.
+
+  - Origin: Web content have a scheme (protocol), host (domain), and port assigned
+  to them, all located in the URL.
+    - Scheme: HTTP, HTTPS
+    - Host: example.com
+    - Port: 80 (Default Port)
+*/
+
+/* Browsers automatically restrict cross-origin HTTP requests. It isn't possible to
+access local files either because they use file pathways rather than URLs. To access
+local files, a webserver needs to be created.*/
